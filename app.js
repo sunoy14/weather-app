8555aae5b893255757698c1f7c3a1cc93a02d8ef
Millo Lailang <sunoy14socmed@gmail.com>
2018-01-20 04:54:32 +0530

    refactored some codes in app.js to weather-app/geocode/geocode.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
-const request = require('request');
+
+var geocode = require('./geocode/geocode.js');
 
 const argv = yargs
 	.options({
@@ -16,20 +17,10 @@ const argv = yargs
 
 console.log(argv);
 
-var encodedUri = encodeURIComponent(argv.address);
-
-request({
-	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUri}`,
-	json: true
-}, (error, response, body) => {
-	if(error){
-		console.log('unable to connect to google servers.');
-	} else if (body.status === 'ZERO_RESULTS'){
-		console.log('Unable to find that address');
-	} else if (body.status === 'OK'){
-		//console.log(JSON.stringify(body, undefined, 4));
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+var geocodeAddress = geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+	if(errorMessage){
+		console.log(errorMessage);
+	} else {
+		console.log(JSON.stringify(results, undefined, 2));
 	}
-});
+});		
